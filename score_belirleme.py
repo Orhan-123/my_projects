89a5ccaaa72811438916bf31fe589f0d63215a9e
Orhan-123 <78277807+Orhan-123@users.noreply.github.com>
2023-07-09 19:55:50 +0100

    Python

---
+++ score_belirleme.py
@@ -0,0 +1,11 @@
+score = int(input("Enter your score: "))
+if score < 100 and score > 80:
+  print("Your grade is A")
+if score < 80 and score > 60:
+  print("Your grade is B")
+if score < 60 and score > 40:
+  print("Your grade is C")
+if score < 40 and score > 20:
+  print("Your grade is D")
+if score < 20 and score > 0:
+  print("Your grade is E")
